536e63812cf2ab98342593c8a154b2351936a5f4
Maria Julieta Vignoli <jvignoli2002@gmail.com>
2024-08-27 10:35:52 -0300

    Resolví bugs del login

--- src/routes/routers/userRouter.js
+++ src/routes/routers/userRouter.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../../middlewares/isAuthenticated.js';
-import { isMyAccount } from '../../middlewares/isMyAccount.js';
 import { Users } from '../../controllers/users/index.js';
-import { PutController } from '../../controllers/users/controllers/PutController.js';
-import { GetController } from '../../controllers/users/controllers/GetController.js';
 import { validateBody } from '../../middlewares/validateBody.js';
 import { post_userValidationSchema } from '../../helpers/validationsSchemas/usersValidationSchema.js';
 
