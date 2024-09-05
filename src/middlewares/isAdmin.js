55a4680acd676b4f3581742826aec6e1f3c89af7
lucas <zlucaschipolariz@gmail.com>
2024-09-05 12:03:35 -0300

    realice el cambio de middleware para permitir que el superadmin tambien pueda hacer cambios

--- src/middlewares/isAdmin.js
+++ src/middlewares/isAdmin.js
@@ -3,7 +3,11 @@ import HttpCodes from 'http-status-codes';
 export const isAdmin = (req, res, next) => {
   const { user } = req;
 
-  if (!user.roles.includes('admin')) {
+  if (
+    !user.roles.some(
+      (role) => role.name === 'admin' || role.name === 'superadmin',
+    )
+  ) {
     res.status(HttpCodes.FORBIDDEN).json({
       data: null,
       message: 'No tienes permiso para acceder a este recurso',
